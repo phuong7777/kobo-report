1dcfff955edb00a99b800f99f1903414558432de
phuong7777 <78539773+phuong7777@users.noreply.github.com>
2026-03-28 15:40:23 +0700

    Update sync_kobo.py

--- sync_kobo.py
+++ sync_kobo.py
@@ -56,10 +56,10 @@ for _, row in choices_df.iterrows():
 
 columns_meta = {}
 
-for f in fields:
-    columns_meta[f["field_output"]] = f["display_name"]
-
-columns_meta["submission_time"] = "Thời gian nộp"
+#for f in fields:
+#    columns_meta[f["field_output"]] = f["display_name"]
+#
+#columns_meta["submission_time"] = "Thời gian nộp"
 
 # =========================
 # XỬ LÝ DATA
